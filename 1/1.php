011a5346f1c532f39ffbb467a92e4b68cb4b67be
Akeda Bagus <admin@gedex.web.id>
2015-02-17 19:40:44 +0700

    Use function in 1.php.

--- 1/1.php
+++ 1/1.php
@@ -1,8 +1,9 @@
 <?php
 
-add_action( 'wp_footer', function( $title ) {
+function _p1_wp_footer() {
 	?>
 	<p><?php echo esc_html( 'This pass PHPCS' ); ?></p>
 	<?php
-} );
+}
 
+add_action( 'wp_footer', '_p1_wp_footer' );
